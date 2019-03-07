0355efaa097bc04bb43d0de24f030b11ea5e2f32
Shivangi Mehta <shivymehta98@gmail.com>
2019-03-07 20:16:55 +0530

    test git repo

--- src/app/manage-instructor/manage-instructor.component.ts
+++ src/app/manage-instructor/manage-instructor.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-instructor.component.css']
 })
 export class ManageInstructorComponent implements OnInit {
+  retrievedData: any = [];
+  elements: any = [
+    this.retrievedData = JSON.parse(localStorage.getItem("info"))
+  ];
+
+  headElements = [ 'Firstname', 'Lastname', 'number', 'city', 'country'];
 
   constructor() { }
 
